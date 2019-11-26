6199bb5cfa1185f8f280615e9f71544a4d2e538b
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-11-25 23:02:17 -0700

    feat: Retrieve similar recipes using RetroFit

--- app/src/main/java/com/example/recipe_q/util/Http.java
+++ app/src/main/java/com/example/recipe_q/util/Http.java
@@ -2,6 +2,7 @@ package com.example.recipe_q.util;
 
 import com.example.recipe_q.BuildConfig;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -13,6 +14,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class Http {
     private static final String API_KEY = "apiKey=" + BuildConfig.API_KEY_SPOONACULAR;
@@ -20,6 +22,12 @@ public class Http {
     private static final String API_HOST = "api.spoonacular.com";
 
     private static final String JSON_TAG_JOKE_TEXT = "text";
+    private static final String JSON_TAG_IDENTIFIER = "id";
+    private static final String JSON_TAG_TITLE = "title";
+    private static final String JSON_TAG_READY_MINUTES = "readyInMinutes";
+    private static final String JSON_TAG_SERVINGS = "servings";
+    private static final String JSON_TAG_IMAGE_MAIN = "image";
+    private static final String JSON_TAG_IMAGE_LIST = "imageUrls";
 
     private Spoonacular mSpoonacular;
     private Listener mListener;
@@ -37,6 +45,10 @@ public class Http {
                 .build();
 
         mSpoonacular = retrofit.create(Spoonacular.class);
+    }
+
+    private void onInternetFailure(Call<ResponseBody> call, Throwable throwable) {
+        mListener.onInternetFailure(throwable);
     }
 
     public void getRandomJoke() {
@@ -60,7 +72,62 @@ public class Http {
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                mListener.onInternetFailure(t);
+                onInternetFailure(call, t);
+            }
+        });
+    }
+
+    public void getRecipesSimilarTo(long id) { getRecipesSimilarTo(Long.toString(id)); }
+    public void getRecipesSimilarTo(String id) {
+        mSpoonacular.getRecipesSimilarTo(id).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                ResponseBody responseBody = response.body();
+                if (responseBody != null) {
+                    try {
+                        String jsonString = responseBody.string();
+                        JSONArray jsonEntire = new JSONArray(jsonString);
+
+                        JSONObject currentObject;
+                        long currentId;
+                        String currentTitle;
+                        int currentReadyMin;
+                        int currentServings;
+                        String currentImage;
+                        JSONArray currentImageUrls;
+                        int currentImageListLength;
+                        String [] currentImageList;
+                        int arraySize = jsonEntire.length();
+                        for (int i = 0; i < arraySize; i++) {
+                            currentObject = jsonEntire.getJSONObject(i);
+                            if (currentObject != null) {
+                                currentId = currentObject.getLong(JSON_TAG_IDENTIFIER);
+                                currentTitle = currentObject.getString(JSON_TAG_TITLE);
+                                currentReadyMin = currentObject.getInt(JSON_TAG_READY_MINUTES);
+                                currentServings = currentObject.getInt(JSON_TAG_SERVINGS);
+                                currentImage = currentObject.getString(JSON_TAG_IMAGE_MAIN);
+                                currentImageUrls = currentObject.getJSONArray(JSON_TAG_IMAGE_LIST);
+
+                                if (currentImageUrls != null) {
+                                    currentImageListLength = currentImageUrls.length();
+                                    currentImageList = new String[currentImageListLength];
+                                    for (int j = 0; j < currentImageListLength; j++) {
+                                        currentImageList[j] = currentImageUrls.getString(j);
+                                    }
+                                }
+                            }
+                        }
+                    } catch (JSONException e) {
+                        // TODO: Handle appropriately
+                    } catch (IOException e) {
+                        // TODO: Handle appropriately
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                onInternetFailure(call, t);
             }
         });
     }
@@ -68,5 +135,8 @@ public class Http {
     private interface Spoonacular {
         @GET("food/jokes/random/?" + API_KEY)
         Call<ResponseBody> getRandomJoke();
+
+        @GET("recipes/{id}/similar/?" + API_KEY)
+        Call<ResponseBody> getRecipesSimilarTo(@Path("id") String recipeId);
     }
 }
