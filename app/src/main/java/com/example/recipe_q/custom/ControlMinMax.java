2d916783e653a6dec2314e2dba7c1e3b58808b49
Karl Arthur <karl.crispen.arthur@gmail.com>
2019-12-26 02:38:03 -0700

    feat: Add minimum-maximum amount dialog and controller

---
+++ app/src/main/java/com/example/recipe_q/custom/ControlMinMax.java
@@ -0,0 +1,120 @@
+package com.example.recipe_q.custom;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.recipe_q.R;
+
+public class ControlMinMax extends LinearLayout implements DialogMinMax.Listener {
+    private DialogMinMax mDialog;
+
+    private String mUnit;
+    private TextView mSummary;
+
+    private String mSummaryComplete;
+    private String mSummaryAtLeast;
+    private String mSummaryAtMost;
+    private String mSummaryExactly;
+    private String mSummaryBetween;
+    private String mSummaryAny;
+
+    public ControlMinMax(Context context) {
+        super(context);
+        init(context, null, 0, 0);
+    }
+
+    public ControlMinMax(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0, 0);
+    }
+
+    public ControlMinMax(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr, 0);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ControlMinMax);
+        String title = a.getString(R.styleable.ControlMinMax_criteriaTitle);
+        mUnit = a.getString(R.styleable.ControlMinMax_criteriaUnit);
+        int titleRes = a.getResourceId(R.styleable.ControlMinMax_criteriaTitle, R.string.empty);
+        int unitRes = a.getResourceId(R.styleable.ControlMinMax_criteriaUnit, R.string.empty);
+        int minLabelRes = a.getResourceId(R.styleable.ControlMinMax_labelMinimum, R.string.empty);
+        int maxLabelRes = a.getResourceId(R.styleable.ControlMinMax_labelMaximum, R.string.empty);
+        int minValueRes = a.getResourceId(R.styleable.ControlMinMax_valueMinimum, R.string.empty);
+        int maxValueRes = a.getResourceId(R.styleable.ControlMinMax_valueMaximum, R.string.empty);
+        a.recycle();
+
+        mDialog = new DialogMinMax(
+                context,
+                this,
+                titleRes,
+                unitRes,
+                minLabelRes,
+                maxLabelRes,
+                minValueRes,
+                maxValueRes
+        );
+
+        Resources res = context.getResources();
+        mSummaryComplete = res.getString(R.string.min_max_summary_restrict_completely);
+        mSummaryAtLeast = res.getString(R.string.min_max_summary_restrict_min);
+        mSummaryAtMost = res.getString(R.string.min_max_summary_restrict_max);
+        mSummaryExactly = res.getString(R.string.min_max_summary_restrict_same);
+        mSummaryBetween = res.getString(R.string.min_max_summary_restrict_both);
+        mSummaryAny = res.getString(R.string.min_max_summary_no_restriction);
+
+        View view = inflate(context, R.layout.item_min_max, this);
+        TextView tvTitle = view.findViewById(R.id.tv_title);
+        tvTitle.setText(title);
+        mSummary = view.findViewById(R.id.tv_item_summary);
+        updateSummary();
+    }
+
+    private void updateSummary() {
+        String minString = mDialog.getMinimum();
+        String maxString = mDialog.getMaximum();
+
+        int minAmount = minString.isEmpty() ? 0 : Integer.parseInt(minString);
+        if (maxString.isEmpty()) {
+            // No maximum
+            if (minAmount > 0) {
+                mSummary.setText(String.format(mSummaryAtLeast, minAmount, mUnit));
+            } else {
+                mSummary.setText(mSummaryAny);
+            }
+        } else {
+            int maxAmount = Integer.parseInt(maxString);
+            if (maxAmount == 0) {
+                mSummary.setText(mSummaryComplete);
+            } else if (maxAmount == minAmount) {
+                mSummary.setText(String.format(mSummaryExactly, minAmount, mUnit));
+            } else if (minAmount == 0) {
+                mSummary.setText(String.format(mSummaryAtMost, maxAmount, mUnit));
+            } else {
+                mSummary.setText(String.format(mSummaryBetween, minAmount, maxAmount, mUnit));
+            }
+        }
+    }
+
+    public DialogMinMax getDialog() {
+        return mDialog;
+    }
+
+    @Override
+    public void onCommit() {
+        updateSummary();
+    }
+
+    @Override
+    public void onCancel() {
+        // Do nothing
+    }
+}
---
+++ app/src/main/java/com/example/recipe_q/custom/DialogMinMax.java
@@ -0,0 +1,125 @@
+package com.example.recipe_q.custom;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.recipe_q.R;
+
+public class DialogMinMax extends DialogFragment {
+    private Listener mListener;
+
+    private String mTitle;
+    private String mUnit;
+    private String mLabelMinimum;
+    private String mLabelMaximum;
+
+    private EditText mEtMinimum;
+    private EditText mEtMaximum;
+
+    private String mValueMinimum;
+    private String mValueMaximum;
+
+    public interface Listener {
+        void onCommit();
+        void onCancel();
+    }
+
+    DialogMinMax(
+            Context context,
+            Listener listener,
+            int titleResource,
+            int unitResource,
+            int minLabelResource,
+            int maxLabelResource,
+            int minValueResource,
+            int maxValueResource
+    ) {
+        mListener = listener;
+        Resources res = context.getResources();
+        mTitle = res.getString(titleResource);
+        mUnit = res.getString(unitResource);
+        mLabelMinimum = res.getString(minLabelResource);
+        mLabelMaximum = res.getString(maxLabelResource);
+        mValueMinimum = res.getString(minValueResource);
+        mValueMaximum = res.getString(maxValueResource);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Activity activity = getActivity();
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.item_dialog_min_max, null);
+        TextView tvMinLabel = dialogView.findViewById(R.id.tv_minimum_label);
+        tvMinLabel.setText(mLabelMinimum);
+        TextView tvMaxLabel = dialogView.findViewById(R.id.tv_maximum_label);
+        tvMaxLabel.setText(mLabelMaximum);
+        TextView tvMinUnit = dialogView.findViewById(R.id.tv_minimum_unit);
+        tvMinUnit.setText(mUnit);
+        TextView tvMaxUnit = dialogView.findViewById(R.id.tv_maximum_unit);
+        tvMaxUnit.setText(mUnit);
+        mEtMinimum = dialogView.findViewById(R.id.et_minimum_amount);
+        mEtMinimum.setText(mValueMinimum);
+        mEtMaximum = dialogView.findViewById(R.id.et_maximum_amount);
+        mEtMaximum.setText(mValueMaximum);
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity())
+                .setTitle(mTitle)
+                .setView(dialogView)
+                .setPositiveButton(R.string.dialog_btn_positive, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        determineValues();
+                        mListener.onCommit();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_btn_negative, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mListener.onCancel();
+                    }
+                })
+        ;
+
+        return dialogBuilder.create();
+    }
+
+    private void determineValues() {
+        String strMinimum = mEtMinimum.getText().toString();
+        int integerMin = strMinimum.isEmpty() ? 0 : Math.abs(Integer.parseInt(strMinimum));
+        String strMaximum = mEtMaximum.getText().toString();
+        if (strMaximum.isEmpty()) {
+            mValueMaximum = "";
+        } else {
+            int integerMax = Math.abs(Integer.parseInt(strMaximum));
+            if (integerMin > integerMax) {
+                int temp = integerMin;
+                integerMin = integerMax;
+                integerMax = temp;
+            }
+            mValueMaximum = Integer.toString(integerMax);
+        }
+        mValueMinimum = Integer.toString(integerMin);
+    }
+
+    String getMinimum() {
+        return mValueMinimum;
+    }
+
+    String getMaximum() {
+        return mValueMaximum;
+    }
+}
