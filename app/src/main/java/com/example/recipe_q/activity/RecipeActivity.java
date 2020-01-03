3632dda514ac42d89909ce61290ac864305e1024
Karl Arthur <karl.crispen.arthur@gmail.com>
2020-01-02 22:31:34 -0700

    feat: Launch original recipe page within browser

--- app/src/main/java/com/example/recipe_q/activity/RecipeActivity.java
+++ app/src/main/java/com/example/recipe_q/activity/RecipeActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,8 @@ import java.util.Locale;
 public class RecipeActivity extends AppCompatActivity {
     public static final String RECIPE_PARCELABLE = "recipe_parcelable_one";
 
+    private Recipe mRecipe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,31 +33,31 @@ public class RecipeActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         if (intent != null) {
-            Recipe recipe = intent.getParcelableExtra(RECIPE_PARCELABLE);
+            mRecipe = intent.getParcelableExtra(RECIPE_PARCELABLE);
 
             ImageView recipeImage = findViewById(R.id.iv_recipe_image);
-            Picasso.get().load(recipe.getImage()).placeholder(R.drawable.ic_launcher_background).into(recipeImage);
+            Picasso.get().load(mRecipe.getImage()).placeholder(R.drawable.ic_launcher_background).into(recipeImage);
 
             ActionBar actionBar = getSupportActionBar();
             if (actionBar != null) {
-                actionBar.setTitle(recipe.getRecipeTitle());
+                actionBar.setTitle(mRecipe.getRecipeTitle());
                 actionBar.setDisplayHomeAsUpEnabled(true);
             }
 
-            AdapterLinearIngredients adapterIngredients = new AdapterLinearIngredients(recipe.getIngredients());
+            AdapterLinearIngredients adapterIngredients = new AdapterLinearIngredients(mRecipe.getIngredients());
             RecyclerView rvIngredients = findViewById(R.id.rv_ingredients);
             rvIngredients.setLayoutManager(new LinearLayoutManager(this));
             rvIngredients.setAdapter(adapterIngredients);
 
-            AdapterLinearDirectionGroups adapterDirections = new AdapterLinearDirectionGroups(recipe.getDirections());
+            AdapterLinearDirectionGroups adapterDirections = new AdapterLinearDirectionGroups(mRecipe.getDirections());
             RecyclerView rvDirections = findViewById(R.id.rv_directions);
             rvDirections.setLayoutManager(new LinearLayoutManager(this));
             rvDirections.setAdapter(adapterDirections);
 
             TextView tvServings = findViewById(R.id.tv_servings_amount);
-            tvServings.setText(String.format(Locale.getDefault(), "%1$.2f", recipe.getServings()));
+            tvServings.setText(String.format(Locale.getDefault(), "%1$.2f", mRecipe.getServings()));
             TextView tvReadyIn = findViewById(R.id.tv_ready_in_minutes);
-            tvReadyIn.setText(String.format(Locale.getDefault(), "%1$d", recipe.getReadyInMinutes()));
+            tvReadyIn.setText(String.format(Locale.getDefault(), "%1$d", mRecipe.getReadyInMinutes()));
             ImageView ivFavorite = findViewById(R.id.iv_favorite);
             ivFavorite.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -80,9 +83,16 @@ public class RecipeActivity extends AppCompatActivity {
             btnShowOriginal.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    launchBrowser();
                 }
             });
+        }
+    }
+
+    private void launchBrowser() {
+        if (mRecipe != null) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mRecipe.getSourceUrl()));
+            startActivity(browserIntent);
         }
     }
 }
