367f3fabfaa5a6fd89a6059696d018834a00fe44
Karl Arthur <karl.crispen.arthur@gmail.com>
2020-01-20 21:16:23 -0700

    fix: Only accept valid input from Firebase

--- app/src/main/java/com/example/recipe_q/service/MessagingService.java
+++ app/src/main/java/com/example/recipe_q/service/MessagingService.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.os.Build;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -22,9 +21,10 @@ import com.google.firebase.messaging.RemoteMessage;
 import java.util.Map;
 
 import static com.example.recipe_q.activity.RecipeActivity.RECIPE_PARCELABLE;
+import static com.example.recipe_q.util.Api.ENDPOINT_FREE_BASE;
 
 public class MessagingService extends FirebaseMessagingService {
-    private static final String TAG = MessagingService.class.getSimpleName();
+    private static final String FIREBASE_RECEPTION_CHANNEL = "Post-reception of Firebase message";
 
     public static final String FIREBASE_LATEST_TOKEN = "fmc_token";
     public static final String FIREBASE_RECEIVED_RECIPE_ID = "reference";
@@ -33,7 +33,6 @@ public class MessagingService extends FirebaseMessagingService {
 
     @Override
     public void onNewToken(@NonNull String s) {
-        Log.v(TAG, "onNewToken: \"" + s + "\"");
         super.onNewToken(s);
         saveToken(s);
     }
@@ -43,14 +42,12 @@ public class MessagingService extends FirebaseMessagingService {
     // to write code to handle the onMessageReceived callback."
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
-        Log.v(TAG, "onMessageReceived");
         super.onMessageReceived(remoteMessage);
         FavoriteRecipe receivedRecipe = parseRecipe(remoteMessage.getData());
         sendNotification(remoteMessage.getNotification(), receivedRecipe);
     }
 
     private FavoriteRecipe parseRecipe(Map<String, String> data) {
-        Log.v(TAG, "parseRecipe");
         FavoriteRecipe recipe = null;
         if (data.size() > 0) {
             // When we receive valid information for creating a favorite-type recipe, store it
@@ -59,18 +56,21 @@ public class MessagingService extends FirebaseMessagingService {
             String image = data.get(FIREBASE_RECEIVED_RECIPE_IMAGE);
             if (reference != null && !reference.isEmpty() &&
                     title != null && !title.isEmpty() &&
-                    image != null && !image.isEmpty()
+                    image != null && !image.isEmpty() && image.startsWith(ENDPOINT_FREE_BASE)
             ) {
-                long id = Long.parseLong(reference);
-                recipe = new FavoriteRecipe(id, title, image);
-                saveRecipe(recipe);
+                try {
+                    long id = Long.parseLong(reference);
+                    recipe = new FavoriteRecipe(id, title, image);
+                    saveRecipe(recipe);
+                } catch (Exception e) {
+                    // No need to do anything; input pushed here by Firebase was invalid.
+                }
             }
         }
         return recipe;
     }
 
     private void saveToken(String newToken) {
-        Log.v(TAG, "saveToken");
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor edit = prefs.edit();
         edit.putString(FIREBASE_LATEST_TOKEN, newToken);
@@ -78,7 +78,6 @@ public class MessagingService extends FirebaseMessagingService {
     }
 
     private void saveRecipe(FavoriteRecipe recipe) {
-        Log.v(TAG, "saveRecipe");
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor edit = prefs.edit();
         edit.putLong(FIREBASE_RECEIVED_RECIPE_ID, recipe.getIdSpoonacular());
@@ -89,7 +88,6 @@ public class MessagingService extends FirebaseMessagingService {
 
     // https://github.com/firebase/quickstart-android/blob/master/messaging/app/src/main/java/com/google/firebase/quickstart/fcm/java/MyFirebaseMessagingService.java
     private void sendNotification(RemoteMessage.Notification notify, FavoriteRecipe recipe) {
-        Log.v(TAG, "sendNotification");
         PendingIntent pending = null;
         if (recipe != null) {
             Intent intent = new Intent(this, RecipeActivity.class);
@@ -128,7 +126,7 @@ public class MessagingService extends FirebaseMessagingService {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notifyChannel = new NotificationChannel(
                     channelId,
-                    "Post-reception of Firebase message",
+                    FIREBASE_RECEPTION_CHANNEL,
                     NotificationManager.IMPORTANCE_DEFAULT
             );
             notifyManager.createNotificationChannel(notifyChannel);
